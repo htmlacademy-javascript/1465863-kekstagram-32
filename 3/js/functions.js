45327eaa774619325bd2fce2b770f1c47ebac727
Keks <keks@htmlacademy.ru>
2024-07-02 19:05:14 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,73 @@
+// Функция проверяет длину строки
+function checkStringLength(inputString, maxLength) {
+  if(inputString.length <= maxLength) {
+    return true;
+  }
+  return false;
+}
+
+<<<<<<< HEAD
+=======
+console.log(checkStringLength('проверяемая строка', 20));
+console.log(checkStringLength('проверяемая строка', 18));
+console.log(checkStringLength('проверяемая строка', 10));
+
+>>>>>>> upstream/master
+// Функция проверяет является ли строка палиндромом
+
+function isPalindrome(inputString) {
+
+  const normalizedString = inputString.replace(/ /g, '').toLowerCase();
+
+  let reversedString = '';
+
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+
+  return normalizedString === reversedString;
+}
+
+<<<<<<< HEAD
+=======
+console.log(isPalindrome('топот'));
+console.log(isPalindrome('ДовОд'));
+console.log(isPalindrome('Кекс'));
+console.log(isPalindrome('Лёша на полке клопа нашёл '));
+>>>>>>> upstream/master
+
+// Функция извлекает числа из строки
+function extractDigits(input) {
+
+  const str = input.toString();
+
+  // Инициализируем переменную для хранения цифр
+  let digits = '';
+
+  // Перебираем каждый символ строки
+  for (let i = 0; i < str.length; i++) {
+    // Проверяем, является ли текущий символ цифрой
+    if (!isNaN(parseInt(str[i]))) {
+      // Если да, добавляем его к digits
+      digits += str[i];
+    }
+  }
+
+  if (digits !== '') {
+    return parseInt(digits);
+  }
+  return NaN;
+}
+
+<<<<<<< HEAD
+=======
+console.log(extractDigits('2023 год'));
+console.log(extractDigits('ECMAScript 2022'));
+console.log(extractDigits('1 кефир, 0.5 батона'));
+console.log(extractDigits('агент 007'));
+console.log(extractDigits('а я томат'));
+
+console.log(extractDigits(2023));
+console.log(extractDigits(-1));
+console.log(extractDigits(1.5));
+>>>>>>> upstream/master
